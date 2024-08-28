f3b79fdc1ff6f3a4c1bcae3724d6aa434c1c3ef3
toonvata <toonvata@gmail.com>
2024-08-28 23:46:19 +0700

    Update question.js

--- api/question.js
+++ api/question.js
@@ -1,9 +1,16 @@
 const ThaiElementAssessment = require('../ThaiElementAssessment');
 
 module.exports = (req, res) => {
-    const assessment = new ThaiElementAssessment();
-    res.json({
-        questions: assessment.getQuestions(),
-        clinical_symptoms: assessment.getClinicalSymptoms()
-    });
+    try {
+        const assessment = new ThaiElementAssessment();
+        const data = {
+            questions: assessment.getQuestions(),
+            clinical_symptoms: assessment.getClinicalSymptoms()
+        };
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error in /api/questions:', error);
+        res.status(500).json({ error: 'Internal Server Error', message: error.message });
+    }
 };
